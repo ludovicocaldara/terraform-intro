b86e246299be2d2f126677fc430851e2ddb80297
Ludovico Caldara - Oracle <80839073+lcaldara-oracle@users.noreply.github.com>
2021-09-16 17:38:16 +0200

    lab4

---
+++ database.tf
@@ -0,0 +1,25 @@
+
+
+resource "random_password" "adb_password" {
+  length           = 20
+  special          = true
+  numeric          = true
+  upper            = true
+  lower            = true
+  override_special = "_%@+!"
+  min_lower        = 2
+  min_numeric      = 2
+  min_special      = 2
+  min_upper        = 2
+}
+
+resource "oci_database_autonomous_database" "demo_adb" {
+    #Required
+    compartment_id = var.compartment_id
+    db_name        = "demo_adb"
+
+    #Optional
+    admin_password = random_password.adb_password.result
+    is_free_tier   = true
+    subnet_id      = oci_core_subnet.demo-public-subnet.id
+}
