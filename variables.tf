e5750971b121e36f46212cb9567268bceb69cc4f
Ludovico Caldara - Oracle <80839073+lcaldara-oracle@users.noreply.github.com>
2021-09-16 15:47:56 +0200

    lab3

--- variables.tf
+++ variables.tf
@@ -6,3 +6,8 @@ variable "vcn_cidr" {
   description = "CIDR block for the VCN. Security rules are created after this."
   default = "10.0.0.0/16"
 }
+
+variable "subnet_cidr" {
+  description = "CIDR block for the subnet."
+  default = "10.0.0.0/24"
+}
