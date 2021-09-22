78fca14832f433f1d8ffa5087ec51ba2c09e90ab
Ludovico Caldara <ludovico.caldara@gmail.com>
2021-09-22 17:31:16 +0000

    lab6

--- bastion_session.tf
+++ bastion_session.tf
@@ -4,7 +4,7 @@ resource "oci_bastion_session" "demo_session" {
         public_key_content = var.ssh_public_key
     }
     target_resource_details {
-        session_type       = "MANAGED_SSH"
+        session_type       = "PORT_FORWARDING"
         target_resource_id = oci_core_instance.demo_vm.id
 
         target_resource_operating_system_user_name = "opc"
